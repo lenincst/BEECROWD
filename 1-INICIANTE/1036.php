0b4cd2bfe507b495ef9815797014acb4a2204393
Lenin Costa da Rosa <lenincostta@gmail.com>
2025-12-08 22:23:52 -0300

    adicionado ex. 1036

--- 1-INICIANTE/1036.php
+++ 1-INICIANTE/1036.php
@@ -6,6 +6,18 @@ $a = $array[0];
 $b = $array[1];
 $c = $array[2];
 
-
+if ($a == 0){
+    echo "Impossivel calcular",PHP_EOL;
+}else{
+    $delta = pow($b,2)-4*$a*($c);
+    if ($delta < 0){
+        echo "Impossivel calcular",PHP_EOL;
+    }else{
+        $r1 = (-$b + sqrt($delta))/(2*$a);
+        $r2 = (-$b - sqrt($delta))/(2*$a);
+        echo "R1 = ".sprintf("%.5f",$r1),PHP_EOL;
+        echo "R2 = ".sprintf("%.5f",$r2),PHP_EOL;
+    }
+}PHP_EOL;
 
 ?>
